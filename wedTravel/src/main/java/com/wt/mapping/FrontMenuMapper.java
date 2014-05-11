357b5f7cc6028822c17df8348df029cd0e5a7b4f
ghouman <houmangong@163.com>
2014-05-11 09:48:10 +0800

    0.09

--- wedTravel/src/main/java/com/wt/mapping/FrontMenuMapper.java
+++ wedTravel/src/main/java/com/wt/mapping/FrontMenuMapper.java
@@ -3,15 +3,6 @@ package com.wt.mapping;
 import com.wt.bean.table.FrontMenu;
 
 public interface FrontMenuMapper {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(FrontMenu record);
-
-    int insertSelective(FrontMenu record);
 
     FrontMenu selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(FrontMenu record);
-
-    int updateByPrimaryKey(FrontMenu record);
 }
